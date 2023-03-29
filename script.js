4ec005fd5cfb157d162ba2de627f827ee94960e8
Tech Geek United <79381496+TechGeekUnited@users.noreply.github.com>
2023-03-29 17:28:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 window.onload = function() {
-	// Get the Base64 identifiers from the URL
-	var identifiers = window.location.search.substring(1).split("&");
+	// Get the Base64 identifier from the URL
+	var identifier = window.location.search.substring(1);
 
-	// Convert the identifiers from Base64 to UTF-8 and add ".html" to the end
-	var decodedIdentifier1 = atob(identifiers[0]) + ".html";
-	var decodedIdentifier2 = atob(identifiers[1]) + ".html";
+	// Convert the identifier from Base64 to UTF-8
+	var decodedIdentifier = atob(identifier);
 
-	// Load the HTML document using the decoded identifiers
+	// Load the HTML document using the decoded identifier
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			document.getElementById("docViewer").innerHTML = this.responseText;
 		}
 	};
-	xhttp.open("GET", decodedIdentifier1 + "?" + decodedIdentifier2, true);
+	xhttp.open("GET", decodedIdentifier, true);
 	xhttp.send();
 };
