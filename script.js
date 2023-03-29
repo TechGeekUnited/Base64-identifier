b0127f97ce9e04b78153292664915b44b65ad2b5
Tech Geek United <79381496+TechGeekUnited@users.noreply.github.com>
2023-03-29 17:43:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,8 @@ window.onload = function() {
 	// Get the value after "?" in the URL
 	var queryString = window.location.search.substring(1);
 
-	// Decode the Base64-encoded query string to UTF-8
-	var decodedQueryString = decodeURIComponent(atob(queryString));
-
-	// Append ".html" to the decoded query string to form the new URL
-	var newUrl = decodedQueryString + ".html";
+	// Append ".html" to the query string to form the new URL
+	var newUrl = queryString + ".html";
 
 	// Use document.location.replace() to navigate to the new URL
 	document.location.replace(newUrl);
